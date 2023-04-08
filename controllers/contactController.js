435d9ddc73aa099f621f776aa0952b6c846b0a2d
kittenmasque <volaju@gmail.com>
2023-04-08 05:55:07 -0700

    added html property for mailOptions to style outgoing emails that will split the message string into an array and rejoin them into a single string using the HTML line break tag <br> at the delimiter to produce new string that keeps the original message's line breaks.

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -11,20 +11,32 @@ router.post('/', async (req, res) => {
     return;
   }
 
-  // Configure your email settings
+  // Configuring email settings
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: process.env.MY_EMAIL, // your email address
-      pass: process.env.MY_PW, // your email password
+      user: process.env.MY_EMAIL, 
+      pass: process.env.MY_PW, 
     },
   });
 
+  //Setting up template for emails sent through Contact form
   const mailOptions = {
     from: email,
-    to: 'volaju@gmail.com', // your email address
+    to: 'volaju@gmail.com', 
     subject: 'New Contact Form Submission',
     text: `Name: ${name}\nEmail: ${email}\n\nMessage:\n${message}`,
+    html: `
+    <div style="font-family: Arial, sans-serif; font-size: 16px; line-height: 1.5; color: #333;">
+      <h1 style="font-size: 24px;">New Contact Form Submission</h1>
+      <p><strong>Name:</strong> ${name}</p>
+      <p><strong>Email:</strong> ${email}</p>
+      <p><strong>Message:</strong></p>
+      <div style="border-left: 3px solid #ccc; padding-left: 10px; margin: 10px 0;">
+        ${message.split('\n').join('<br>')}
+      </div>
+    </div>
+  `,
   };
 
   try {
