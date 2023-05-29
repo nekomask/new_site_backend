b36a1097157b3dea6db6c03a2840d1f4ae8875a0
kittenmasque <volaju@gmail.com>
2023-05-29 08:54:50 -0700

    updated geolocationController to check if IP is valid and added validIP as part of the API fetch

--- controllers/geolocationController.js
+++ controllers/geolocationController.js
@@ -3,14 +3,19 @@ const router = express.Router();
 const axios = require('axios');
 
 router.get('/', async (req, res) => {
-    try {
-    const response = await axios.get(`https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.MY_GEO_API_KEY}`);
+    if (!req.clientIp) {
+        return res.status(400).json({ error: 'Invalid IP address' });
+      }
+    
+
+  try {
+    const response = await axios.get(`https://api.ipgeolocation.io/ipgeo?apiKey=${process.env.MY_GEO_API_KEY}&ip=${req.clientIp}`);
     const { city, state_prov } = response.data;
     res.json({ city, state_prov });
-} catch (error) {
+  } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'An error occurred while fetching geolocation data' });
-}
+  }
 })
 
 module.exports = router;
